fc26ade6fc48ee22571ce21e723b320cf74c467e
THEOFILO GABRIEL BRITO PASSOS <theofilo_passos@estudante.sesisenai.org.br>
2024-06-04 21:21:48 -0300

    Savepoint

---
+++ Exercicio.extra/src/exercicio/extra/ExercicioExtra.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio.extra;
+
+/**
+ *
+ * @author theofilo_passos
+ */
+public class ExercicioExtra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        
+        int aux = A;
+        A = B;
+        B = aux;
+        
+        System.out.println("Valor de A:" + A);
+        System.out.println("Valor de B:" + B);
+        
+        
+    }
+    
+}
